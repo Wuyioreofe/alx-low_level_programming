46e5f9e83cdb1a5ac0631d75042420c0b85d7697
Wuyioreofe <ojekalestephen@gmail.com>
2023-03-25 18:02:12 -0700

    now know betty documentation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+
 /**
-* main - entry point
-* return: Always 0 at success
+* main - Entry point
+* Description: This function prints the string ""with proper grammar, but the
+* outcome is a piece of art", followed by a new line character, to the console.
+* Return: Always returns 0 to indicate success
 */
 int main(void)
 {
